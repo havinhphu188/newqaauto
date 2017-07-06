c02119aed7e19a10accccf171caed7c5a3d25dd7
Hà Vĩnh Phú <havinhphu188>
2017-07-06 11:09:56 +0700

    original

---
+++ src/main/java/com/csc/myqaautomation/App.java
@@ -0,0 +1,13 @@
+package com.csc.myqaautomation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
